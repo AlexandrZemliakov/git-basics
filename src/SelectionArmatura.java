12ff86ac887a41d2b5fb2f754c0e924b8b90a228
Alexandr <alexandr.zemliakov@mail.ru>
2021-06-28 23:38:06 +0300

    delete Fig.java

--- src/SelectionArmatura.java
+++ src/SelectionArmatura.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class SelectionArmatura {
-        public static void main (String[] args){
+        public void main(String[] args){
 
             Scanner scan = new Scanner(System.in);
             System.out.println("Введите диаметр клапана ");
@@ -12,5 +12,10 @@ public class SelectionArmatura {
             System.out.println("Для DN" + dn + ", PN " + pn + " назначение " + " выбюраны следующие позиции:");
 
 
+            System.out.println(car(1,2,5));
+        }
+        public int car (int a, int b, int c){
+              return car(a + b, b = c, c = 1);
         }
 }
+

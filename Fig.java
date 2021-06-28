6e162be9289f914a280fcf6d0d416a8bbeae8da0
AlexandrZemliakov <86569963+AlexandrZemliakov@users.noreply.github.com>
2021-06-28 23:34:40 +0300

    Create Fig.java

---
+++ Fig.java
@@ -0,0 +1,14 @@
+public class Fig {
+        public static void fig (String[] args){
+
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите диаметр клапана ");
+            int dn = scan.nextInt();
+            System.out.println("Введите номинальное давление бар ");
+            int pn = scan.nextInt();
+
+            System.out.println("Для DN" + dn + ", PN " + pn + " назначение " + " выбюраны следующие позиции:");
+
+
+        }
+}
